ab4dbd4107175fe5447df9fa400f49b18806ac4f
Ahmet Simsek <iamaroott@gmail.com>
2017-03-16 00:09:51 +0200

    Update babel.js
    transform-runtime

--- config/babel.js
+++ config/babel.js
@@ -1,9 +1,16 @@
 module.exports = {
-	presets: [
-		['es2015', {loose: true, modules: false}],
-		'stage-2'
-	],
-	plugins: [
-		['transform-react-jsx', {pragma: 'h'}]
-	]
+  passPerPreset: true,
+  presets: [
+    { "plugins": [ "transform-runtime" ] },
+    {
+      "passPerPreset": false,
+      "presets": [
+        ["es2015", {loose: true, modules: false}],
+        "stage-2"
+      ]
+    }
+  ],
+  plugins: [
+    ['transform-react-jsx', {pragma: 'h'}]
+  ]
 };
